abe57413e64d4d0bae861b6b8094e33e649ccb9a
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-06-30 16:32:30 +0900

    fix: safe mlx

---
+++ src/safe_mlx.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   safe_mlx.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/30 14:28:11 by yshimoda          #+#    #+#             */
+/*   Updated: 2023/06/30 15:16:10 by yshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include "mlx.h"
+
+void	*safe_mlx_new_window(void *mlx_ptr, int size_x, int size_y, char *title)
+{
+	void	*win_ptr;
+
+	win_ptr = mlx_new_window(mlx_ptr, size_x, size_y, title);
+	if (!win_ptr)
+		exit_error("mlx new window is failed\n", false);
+	return (win_ptr);
+}
+
+void	*safe_mlx_init(void)
+{
+	void	*mlx_ptr;
+
+	mlx_ptr = mlx_init();
+	if (!mlx_ptr)
+		exit_error("mlx init is failed\n", false);
+	return (mlx_ptr);
+}
